0dd8a819031e593e10613154ed00d3ac56d6f42c
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-04-23 13:18:11 -0400

    advanced GUI

--- tkinterdmeo.py
+++ tkinterdmeo.py
@@ -1,29 +1,39 @@
-try:
-    import tkinter
-except ImportError:
-    import Tkinter as tkinter
+import tkinter
+
 
 main_window = tkinter.Tk()
 
 main_window.title("FUck you")
-main_window.geometry("640x480")
+main_window.geometry("640x480-8-200")
 
-label = tkinter.Label(main_window, text = "Hello World")
-label.pack(side='top')
+label = tkinter.Label(main_window, text="Hello World")
+label.grid(row=0, column=0)
 
 left_frame = tkinter.Frame(main_window)
-left_frame.pack(side='left', anchor='n', fill=tkinter.Y, expand = False)
+left_frame.grid(row=1, column=1)
 
 
-canvas = tkinter.Canvas(main_window, relief= 'raised', borderwidth=1)
-canvas.pack(side='left', anchor = 'n')
+canvas = tkinter.Canvas(main_window, relief='raised', borderwidth=1)
+canvas.grid(row=1, column=1)
 
 right_frame = tkinter.Frame(main_window)
-right_frame.pack(side='right', anchor='n', expand = True)
-button1 = tkinter.Button(right_frame,  text = "button1")
-button2 = tkinter.Button(right_frame,  text = "button2")
-button3 = tkinter.Button(right_frame,  text = "button3")
-button1.pack(side='top')
-button2.pack(side='top')
-button3.pack(side='top')
+right_frame.grid(row=1, column=2, sticky='n')
+button1 = tkinter.Button(right_frame,  text="button1")
+button2 = tkinter.Button(right_frame,  text="button2")
+button3 = tkinter.Button(right_frame,  text="button3")
+button1.grid(row=0, column=0)
+button2.grid(row=1, column=0)
+button3.grid(row=2, column=0)
+
+main_window.columnconfigure(0, weight=1)
+main_window.columnconfigure(1, weight=1)
+main_window.grid_columnconfigure(2, weight=1)
+
+left_frame.config(relief = 'sunken', borderwidth = 1)
+right_frame.config(relief = 'sunken', borderwidth = 1)
+left_frame.grid(sticky = 'ns')
+right_frame.grid(sticky = 'new')
+
+right_frame.columnconfigure(0, weight  = 1)
+button2.grid(sticky= 'ew')
 main_window.mainloop()
