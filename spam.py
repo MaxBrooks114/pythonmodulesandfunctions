660cf2ef3a109a842cf4a4aba347aea4c2f33710
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-04-29 10:57:48 -0400

    nested functions

---
+++ spam.py
@@ -0,0 +1,18 @@
+def spam1():
+
+    def spam2():
+
+        def spam3():
+
+            z = " even more spam"
+            return z
+
+        y = " more spam"
+        y += spam3()
+        return y
+
+    x = "spam"
+    x += spam2()
+    return  x
+
+print(spam1())
